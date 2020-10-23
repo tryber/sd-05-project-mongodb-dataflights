d8019bc17b0e72f98bfb9efc1ce8b06310e84fe8
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-10-23 13:49:15 -0300

    passando no item 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,6 +1,16 @@
-db.voos.find({
-  $and: [
-    {"empresa.nome": 'PASSAREDO'},
-    {natureza: 'Doméstica'},
-  ]
-}).count();
+const empresa = "PASSAREDO";
+
+const natureza = "Doméstica";
+
+const totalVoosDomesticos = db.voos.find({ $and: [{"empresa.nome": empresa}, {natureza: natureza}]}).count();
+
+db.resumoVoos.insertOne({
+  empresa,
+  totalVoosDomesticos
+});
+
+db.resumoVoos.findOne({
+  empresa
+}, {
+  _id: 0
+});
