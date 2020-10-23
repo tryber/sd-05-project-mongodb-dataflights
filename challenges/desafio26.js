8776a77d04c15424ee6cda5f9fada7406e2c7314
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-23 07:34:02 -0300

    cc

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,15 +1,3 @@
-db.voos.count({
-  $and: [
-    { "empresa.nome": "GOL" },
-    {
-      $and: [
-        { "passageiros.pagos": { $gte: 5 } },
-        { "passageiros.pagos": { $lte: 10 } },
-      ],
-    },
-  ],
-});
-
 db.voos.deleteMany({
   $and: [
     { "empresa.nome": "GOL" },
