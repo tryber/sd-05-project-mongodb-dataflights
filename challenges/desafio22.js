4f93c28c4df4d83784c3eb001925fbfead750012
Douglas <douglaza@gmail.com>
2020-10-23 12:07:29 -0300

    desafio22

---
+++ challenges/desafio22.js
@@ -0,0 +1,19 @@
+// Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES, a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK
+db.voos
+  .find(
+    {
+      $and: [
+        {
+          $or: [
+            {
+              "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+            },
+          ],
+        },
+        { "aeroportoOrigem.sigla": "SBGR" },
+        { "aeroportoDestino.sigla": "KJFK" },
+      ],
+    },
+    { vooId: 1 }
+  )
+  .limit(1);
