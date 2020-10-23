c99890e7a2b3272c5f4c64971c83d9a3604f34ba
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-23 15:51:54 -0300

    codigo refatorado por causa do codeclimate

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,8 +1,10 @@
+totalVoos = db.voos.count({
+  "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica"
+});
+
 db.resumoVoos.insertOne({
   empresa: "LATAM AIRLINES BRASIL",
-  totalVoosDomesticos: db.voos.count({
-    "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica"
-  }),
+  totalVoosDomesticos: totalVoos,
 });
 
 db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0 });
