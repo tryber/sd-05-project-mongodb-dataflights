9ba72b1b6dc67ff4ea7c776be7e46d9105ed29fe
hleoc <informig@gmail.com>
2020-10-23 19:13:56 -0300

    Desafio vigésimo segundo

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,1 @@
-db.voos.findOne({}, {_id: 0, vooId: 1});
+db.voos.findOne({$or: [{"empresa.nome": {$eq: "DELTA AIRLINES"}}, {"empresa.nome": {$eq: "AMERICAN AIRLINES"}}, {$and: [{"aeroportoOrigem.sigla": {$eq: "SBGR"}}, {"aeroportoDestino.sigla": {$eq: "KJFK"}}]}]}, {_id: 0, vooId: 1});
