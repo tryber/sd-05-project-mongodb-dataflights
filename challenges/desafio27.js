239789c1f4f24f457fc75c5c122dabb3dea04412
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-23 08:46:53 -0300

    cc

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,7 +1,3 @@
-db.voos.count({
-  $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }],
-});
-
 db.resumoVoos.insertOne({
   empresa: "PASSAREDO",
   totalVoosDomesticos: db.voos.count({
