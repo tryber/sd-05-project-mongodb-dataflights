3ec0cdf40e2d4406899a302103005bc9f1339ac7
Felipe Vieira <flpvieira86@gmail.com>
2020-10-23 16:56:45 -0300

    CC meu amigo, sentirei muito sua falta quando voce for embora...

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -4,11 +4,11 @@
 // Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja LATAM AIRLINES BRASIL.
 
 db.resumoVoos.insertOne({
-  empresa: 'LATAM AIRLINES BRASIL',
   totalVoosDomesticos: db.voos.count({
     'empresa.nome': 'LATAM AIRLINES BRASIL',
     natureza: 'Doméstica',
   }),
+  empresa: 'LATAM AIRLINES BRASIL',
 });
 
 db.resumoVoos.findOne({
