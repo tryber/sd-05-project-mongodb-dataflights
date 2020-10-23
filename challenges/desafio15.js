2130cf63df7eea6906d9e74b78f786eb5c1ca4a8
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-23 18:46:15 -0300

    challenges 15 to 17

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,1 +1,3 @@
 // Retorne o total de voos com mais de 20 decolagens.
+
+db.voos.count({ decolagens: { $gt: 20 } });
--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,1 +1,3 @@
 // Retorne o total de voos em que o campo natureza possui o valor Internacional.
+
+db.voos.count({ natureza: "Internacional" });
