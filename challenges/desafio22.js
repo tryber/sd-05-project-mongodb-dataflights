f616c293d67a168668fb8f70d267aa05b34baf7a
Andrevlc <andrevezi@gmail.com>
2020-10-23 13:17:00 -0300

    Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,9 @@
+db.voos.find( {
+  $and: [
+  { $or:[ { "empresa.nome": { $eq:"DELTA AIRLINES"}} ,{"empresa.nome": {$eq:"AMERICAN AIRLINES"}}
+]},
+{"aeroportoOrigem.sigla":{$eq:"SBGR"}},
+{"aeroportoDestino.sigla":{$eq:"KJFK"}} 
+]},{_id:0,vooId:1}).limit(1);
+
+
