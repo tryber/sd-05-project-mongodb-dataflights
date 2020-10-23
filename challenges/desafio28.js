51259449104d10d29ecbcc5444e508afa94cd657
Christian Duessel <duessel@gmail.com>
2020-10-23 19:31:16 -0300

    req28, libera aí CC

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -4,4 +4,4 @@ db.resumoVoos.insertOne({empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: 
 
 db.resumoVoos.findOne({"empresa": "LATAM AIRLINES BRASIL"},{"_id": 0, "empresa": 1, "totalVoosDomesticos": 1});
 
-// TRANSPARENCIA: PARA REFATORAR ESSE CÓDIGO APRENDI COM O SIDNEY RAMOS A USAR UMA VARIÁVEL
+// TRANSPARENCIA: PARA REFATORAR ESSE CÓDIGO APRENDI COM O SIDNEI RAMOS A USAR UMA VARIÁVEL
