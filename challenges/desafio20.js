3751ee6bd18ea63aa26001dd904b0bd7abb4b7cf
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-23 19:04:03 -0300

    challenges 19 to 21

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,4 @@
 // Retorne o vooId do primeiro voo em que o campo rtk não exista.
+
+db.voos.findOne({ rtk: { $exists: false } }, { vooId: 1, _id: 0 });
+// Funciona também escrever 0 no lugar de false.
