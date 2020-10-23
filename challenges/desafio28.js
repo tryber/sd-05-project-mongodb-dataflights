0d8b9833569e380723064925cca8e0b7e046781c
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-23 08:38:43 -0300

    resumoVoos

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,6 +1,11 @@
-db.voos.count({
-  $and: [
-    { natureza: "Doméstica" },
-    { "empresa.nome": "LATAM AIRLINES BRASIL" },
-  ],
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: db.voos.count({
+    $and: [
+      { natureza: "Doméstica" },
+      { "empresa.nome": "LATAM AIRLINES BRASIL" },
+    ],
+  }),
 });
+
+db.resumoVoos.findOne({}, { _id: 0 });
