dd4f36ecd728b40121909e7579b777801e4cfeee
Christian Duessel <duessel@gmail.com>
2020-10-23 19:28:31 -0300

    req28, ACABOU?!

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,7 @@
-db.resumoVoos.insertOne({empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: db.voos.count({$and: [{"empresa.nome": "LATAM AIRLINES BRASIL"}, {"natureza": "Doméstica"}]})});
+variavel = db.voos.count({$and: [{"empresa.nome": "LATAM AIRLINES BRASIL"}, {"natureza": "Doméstica"}]});
+
+db.resumoVoos.insertOne({empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: variavel});
+
 db.resumoVoos.findOne({"empresa": "LATAM AIRLINES BRASIL"},{"_id": 0, "empresa": 1, "totalVoosDomesticos": 1});
+
+// TRANSPARENCIA: PARA REFATORAR ESSE CÓDIGO APRENDI COM O SIDNEY RAMOS A USAR UMA VARIÁVEL
