b85f7b72739954e515454bec3fbf69e400477e67
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-23 18:51:32 -0300

    challenge 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,1 +1,7 @@
 // Retorne o vooId, mes e ano do primeiro voo com mais de 7000 passageiros pagos.
+
+db.voos.findOne(
+  { "passageiros.pagos": { $gt: 7000 } },
+  { vooId: 1, mes: 1, ano: 1, _id: 0 }
+);
+// Também era possivel manter o find com um limit(1) no final, equivalente do findOne
---
+++ challenges/desafio20.js
@@ -0,0 +1,1 @@
+// Retorne o vooId do primeiro voo em que o campo rtk não exista.
