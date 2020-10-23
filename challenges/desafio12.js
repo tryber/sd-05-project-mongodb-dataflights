4d7953e1bc65bd798f4fa81671faebdf794f3487
Christian Duessel <duessel@gmail.com>
2020-10-23 15:44:39 -0300

    req 13

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,3 @@
 db.voos.count({$or: [{"aeroportoDestino.pais": "BRASIL"}, {"aeroportoDestino.pais": "ARGENTINA"}, {"aeroportoDestino.pais": "CHILE"}]});
+
+// MAIS ESPERTO: db.voos.count({"aeroportoDestino.pais": {$in:["BRASIL", "ARGENTINA", "CHILE"]}})
