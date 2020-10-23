f410bcceb1d55ffcdb1bc61dc25bccc60ef468be
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-23 19:34:47 -0300

    challenge 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,16 @@
-// Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES, a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK.
+// Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES *OU* AMERICAN AIRLINES, *AND* a sigla do aeroporto de origem seja SBGR *AND* a sigla do aeroporto de destino seja KJFK.
+
+db.voos.findOne(
+  {
+    $and: [
+      {
+        $or: [
+          { "empresa.nome": "DELTA AIRLINES" },
+          { "empresa.nome": "AMERICAN AIRLINES" },
+        ],
+      },
+    ],
+  },
+  { "aeroportoOrigem.sigla": "SBGR" },
+  { "aeroportoDestino.sigla": "KJFK" }
+);
