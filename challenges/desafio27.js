d32483feae3ee1b6f5200624dc68a5a6bcddefdc
pedrohcalado <pedrocalado22@gmail.com>
2020-10-23 17:19:33 -0300

    Tentativa de ajustar codeclimate incluindo uma funcao que adiciona os dados na collection

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,2 +1,4 @@
-db.resumoVoos.insert({"empresa": "PASSAREDO" , "totalVoosDomesticos": db.voos.count({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"})});
+import { insertDB } from './desafio27';
+
+insertDB("PASSAREDO", "Doméstica");
 db.resumoVoos.find({empresa: "PASSAREDO"}, {empresa: 1, totalVoosDomesticos: 1, _id: 0});
