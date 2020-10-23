cf7d1ebeb5ed665861f4af97afbf46007416d648
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-10-23 15:07:35 -0300

    Testando teorias 1, dividir codigo em linhas

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -2,18 +2,6 @@ const empresa = "LATAM AIRLINES BRASIL";
 
 const natureza = "Doméstica";
 
-db.resumoVoos.insertOne({
-  empresa,
-  totalVoosDomesticos: db.voos
-  .find({ $and: [{ "empresa.nome": empresa }, { natureza: natureza }] })
-  .count()
-});
+db.resumoVoos.insertOne({ empresa, totalVoosDomesticos: db.voos .find({ $and: [{ "empresa.nome": empresa }, { natureza: natureza }] }) .count()});
 
-db.resumoVoos.findOne(
-  {
-    empresa,
-  },
-  {
-    _id: 0,
-  }
-);
+db.resumoVoos.findOne({ empresa }, { _id: 0, });
