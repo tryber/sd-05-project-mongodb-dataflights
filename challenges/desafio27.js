cfd91bed5d7ba4fa913dc8a0f877723c30142859
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-23 08:45:54 -0300

    27, returning counting directly through the key

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,3 +1,12 @@
 db.voos.count({
   $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }],
 });
+
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({
+  $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }],
+}),
+});
+
+db.resumoVoos.findOne({}, { _id: 0 });
