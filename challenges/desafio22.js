5957b7021aa80ffc0350521d52910e8f79c5543f
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-23 14:11:54 -0300

    desafio 22 feito

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,12 +1,15 @@
-db.voos.findOne({
-  and:[
-    {
-      $or:[
-        {"empresa.nome":"DELTA AIRLINES"},
-        {"empresa.nome":"AMERICAN AIRLINES"},
-      ],  
-    },
-    {"aeroportoOrigem.sigla":"SBGR"},
-    {"aeroportoDestino.sigla":"KJFK"}
-  ]
-},{_id:0, vooId:1} );
+db.voos.findOne(
+  {
+    $and: [
+      {
+        $or: [
+          { "empresa.nome": "DELTA AIRLINES" },
+          { "empresa.nome": "AMERICAN AIRLINES" },
+        ],
+      },
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  { vooId: 1, _id: 0 }
+);
