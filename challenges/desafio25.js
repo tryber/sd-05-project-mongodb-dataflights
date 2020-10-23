a51c55249de026b5831bc4befab052548e8ab295
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-23 20:24:26 -0300

    challenges 25-26 solved without counting deleted docs

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,1 +1,5 @@
-// Remova todos os voos da empresa AZUL em que a quantidade de combustível seja menor do que 400. Informe a quantidade de documentos removidos.
+// Remova todos os voos da empresa AZUL em que a quantidade de combustível seja menor do que 400. // // // Informe a quantidade de documentos removidos -> ?
+
+db.voos.deleteMany({
+  $and: [{ litrosCombustivel: { $lt: 400 } }, { "empresa.nome": "AZUL" }],
+});
--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,10 @@
-// Remova todos os voos da empresa GOL em que a quantidade de passageiros pagos esteja entre 5 e 10, incluindo os casos em que a quantidade é 5 e 10. Informe a quantidade de documentos removidos.
+// Remova todos os voos da empresa GOL em que a quantidade de passageiros pagos esteja entre 5 e 10, incluindo os casos em que a quantidade é 5 e 10.
+// Informe a quantidade de documentos removidos -> ?
+
+db.voos.deleteMany({
+  $and: [
+    { litrosCombustivel: { $exists: true } },
+    { "passageiros.pagos": { $gte: 5 } },
+    { "passageiros.pagos": { $lte: 10 } },
+  ],
+});
