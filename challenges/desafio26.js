17bce3353a0e3f2457522ab60e57f108cf44991a
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-23 20:31:41 -0300

    fixed challenge 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -3,7 +3,7 @@
 
 db.voos.deleteMany({
   $and: [
-    { litrosCombustivel: { $exists: true } },
+    { "empresa.nome": "GOL" },
     { "passageiros.pagos": { $gte: 5 } },
     { "passageiros.pagos": { $lte: 10 } },
   ],
