81c408bbae468328cc9af9d8e8511704e9ccaa4b
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-10-23 13:49:37 -0300

    passando no ultimo desafio

---
+++ challenges/desafio28.js
@@ -0,0 +1,14 @@
+const empresa = "LATAM AIRLINES BRASIL";
+const natureza = "Doméstica";
+const totalVoosDomesticos = db.voos.find({ $and: [{"empresa.nome": empresa}, {natureza: natureza}]}).count();
+
+db.resumoVoos.insertOne({
+  empresa,
+  totalVoosDomesticos
+});
+
+db.resumoVoos.findOne({
+  empresa
+}, {
+  _id: 0
+});
