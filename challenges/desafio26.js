bbf0acdce68ce96a475d9c481fd8f98c83dd3ced
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-10-24 18:20:11 -0300

    Add comentários desafio 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,5 +1,5 @@
 // Remova todos os voos da empresa GOL em que a quantidade de passageiros pagos esteja entre 5 e 10, incluindo os casos em que a quantidade é 5 e 10.
-//  Informe a quantidade de documentos removidos.
+//  Informe a quantidade de documentos removidos - o comando de delete já mostra a quantidade de itens excluídos
 // Atenção às quantidades menores que 10 e maiores que 5
 db.voos.deleteMany({
   $and: [
