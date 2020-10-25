cc2e3c151f494e2b911e2e2711d72f15e04c324b
marinabrcls <barcelos.marina@gmail.com>
2020-10-25 17:48:51 -0300

    ajustezinho no 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,6 @@
 // Retorne o vooId do décimo ao décimo segundo documento da coleção voos.
 
-db.voos.find({}, { vooId: 1, _id: 0 }).limit(3).skip(9);
+db.voos.find({}, {
+  vooId: 1,
+  _id: 0
+}).limit(3).skip(9);
