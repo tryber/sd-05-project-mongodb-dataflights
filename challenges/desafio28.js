8586530a15fc7086966c8ad4d6106c3ba4a0a97f
pedrohcalado <pedrocalado22@gmail.com>
2020-10-26 09:40:55 -0300

    Tentativa de ajustar codeclimate 2

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,2 @@
-db.resumoVoos.insert({"empresa": "LATAM AIRLINES BRASIL" , "totalVoosDomesticos": db.voos.count({"empresa.nome": "LATAM AIRLINES BRASIL", "natureza": "Doméstica"})});
+db.resumoVoos.insert({"empresa": "LATAM AIRLINES BRASIL" , "totalVoosDomesticos": db.voos.count({$and: [{"empresa.nome": "LATAM AIRLINES BRASIL"}, {"natureza": "Doméstica"}]})});
 db.resumoVoos.find({empresa: "LATAM AIRLINES BRASIL"}, {empresa: 1, totalVoosDomesticos: 1, _id: 0});
