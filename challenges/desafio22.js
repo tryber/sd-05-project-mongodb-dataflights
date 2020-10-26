129b0dd53b5422a40c5888e4303678e50b485764
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-10-26 15:00:58 -0300

    Aspas duplas > aspas simples (desafio 22)

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,16 +1,17 @@
 // Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES,
 // a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK.
+// Lembrar das aspas duplas
 db.voos.findOne(
   {
     $and: [
       {
         $or: [
-          { 'empresa.nome': 'DELTA AIRLINES' }, 
-          { 'empresa.nome': 'AMERICAN AIRLINES' }
+          { "empresa.nome": "DELTA AIRLINES" }, 
+          { "empresa.nome": "AMERICAN AIRLINES" }
         ],
       },
-      { 'aeroportoOrigem.sigla': 'SBGR' },
-      { 'aeroportoDestino.sigla': 'KJFK' },
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
     ],
   },
   { vooId: 1, _id: 0 },
