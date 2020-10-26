30ffa30f1b0c0af89f7d1c2f7f3b42effc954ef1
Preto115 <preto115@gmail.com>
2020-10-26 14:11:13 -0300

    tentando me acertar com o CC

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,4 +1,4 @@
 // Desafio 20
 // Retorne o vooId do primeiro voo em que o campo rtk não exista.
 
-db.voos.find({"rtk": {$exists: false}}, {"vooId": 1, "_id": 0} ).limit(1);
+db.voos.find({ rtk: { $exists: false } }, { vooId: 1, _id: 0 }).limit(1);
