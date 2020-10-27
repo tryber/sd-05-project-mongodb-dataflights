46cd155417067e591b7993b14c365dff8c60e4c4
Mau <mauricio.sdjr@gmail.com>
2020-10-27 20:06:12 -0300

    corrigindo desafio 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,6 +1,6 @@
 db.resumoVoos.insertOne({
   "empresa": "PASSAREDO",
-  "totalVoosDomesticos": db.voos.count({"empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"})
+  "totalVoosDomesticos": db.voos.count({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"})
 });
 
 db.resumoVoos.findOne(
