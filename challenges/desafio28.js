3604dee20fba3920968620a522fc83970c7f9431
Mau <mauricio.sdjr@gmail.com>
2020-10-27 19:44:28 -0300

    codeclimate

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,6 +1,6 @@
 db.resumoVoos.insertOne({
   "empresa": "LATAM AIRLINES BRASIL",
-  "totalVoosDomesticos": db.voos.count({"natureza": "Doméstica"}, {"empresa.nome": "LATAM AIRLINES BRASIL"})
+  "totalVoosDomesticos": db.voos.count({ $and: [{"empresa.nome": "LATAM AIRLINES BRASIL"}, {"natureza": "Doméstica"}]})
 });
 
 db.resumoVoos.findOne(
