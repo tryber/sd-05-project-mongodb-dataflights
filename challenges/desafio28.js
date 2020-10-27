9124f267fdeca8520d92943eed1d60f4f6871769
ferrari <luisdiasferrari@gmail.com>
2020-10-26 21:21:02 -0300

    Desafio 27 AND Desafio 28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,3 +1,3 @@
-db.resumoVoos.insertOne({"empresa": "LATAM AIRLINES BRASIL","totalVoosDomesticos": db.voos.count({"empresa.nome": "LATAM AIRLINES BRASIL", "natureza": "Doméstica"}) });
+db.resumoVoos.insertOne({"empresa": "LATAM AIRLINES BRASIL", "totalVoosDomesticos": db.voos.count({ $and: [{"empresa.nome": "LATAM AIRLINES BRASIL"}, {"natureza": "Doméstica"}] }) });
 
-db.resumoVoos.find({"empresa": "LATAM AIRLINES BRASIL"}, {"_id":0, "empresa": 1, "totalVoosDomesticos": 1});
+db.resumoVoos.find({"empresa": "LATAM AIRLINES BRASIL"}, {"_id":0, "empresa": 1, "totalVoosDomesticos": 1}).pretty();
