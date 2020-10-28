f7bbc5980b0d9497e1083d5d24166c79472a9cf4
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-10-28 16:09:21 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,1 @@
+db.voos.count({ $and: [{ "aeroportoDestino.continente": { $ne: "EUROPA" } }, { "aeroportoDestino.continente": { $ne: "ÁSIA" } }, { "aeroportoDestino.continente": { $ne: "OCEANIA" } }] });
