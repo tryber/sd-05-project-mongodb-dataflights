35b8a3ef79056b1eecee28bab5835db0acf481ff
pauloricardoz <trybe.przt@gmail.com>
2020-10-30 11:12:36 -0300

    cc 2

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,3 @@
-db.resumoVoos.insert({"empresa": "LATAM AIRLINES BRASIL", "totalVoosDomesticos": db.voos.count({"empresa.nome":"LATAM AIRLINES BRASIL", "natureza" : "Doméstica"})});
+/* db.resumoVoos.insert({"empresa": "LATAM AIRLINES BRASIL", "totalVoosDomesticos": db.voos.count({"empresa.nome":"LATAM AIRLINES BRASIL", "natureza" : "Doméstica"})});
 db.resumoVoos.find({},{"_id":0});
+ */
