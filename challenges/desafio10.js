63fb6fe49e232ae5eb5faf002bde7637747d9419
Marylange <marylangess@gmail.com>
2020-10-31 16:12:09 -0300

    R10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,4 +2,20 @@
 // voos da empresa GOL do ano de 2017. 
 // Exiba apenas os campos vooId, empresa.nome, 
 // aeroportoOrigem.nome, aeroportoDestino.nome, mes e ano.
-
+db.voos.find(
+  {
+    $and:
+      [
+        { "empresa.nome": "GOL" },
+        { "ano": 2017 }
+      ]
+  },
+  {
+    "vooId": 1,
+    "empresa.nome": 1,
+    "aeroportoOrigem.nome": 1,
+    "aeroportoDestino.nome": 1,
+    "mes": 1,
+    "ano": 1
+  }
+).limit(10);
