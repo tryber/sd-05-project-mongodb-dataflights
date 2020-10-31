e440198fd62bfb6e111a7d6ee1665bc7362b13d4
Marylange <marylangess@gmail.com>
2020-10-31 19:50:30 -0300

    Requisito 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,4 +1,17 @@
-db.voos.find(
-  { "empresa.nome": "PASSAREDO" }
+const voosDomesticos = db.voos.count(
+  {
+    $and: [
+      { "empresa.nome": "PASSAREDO" },
+      { "natureza": "Doméstica" }
+    ]
+  }
 );
-db.createCollection("resumoVoos", { "natureza": "Doméstica" } );
+
+db.resumoVoos.insertOne(
+  { "empresa": "PASSAREDO", "totalVoosDomesticos": voosDomesticos }
+);
+
+db.resumoVoos.find(
+  { "empresa": "PASSAREDO" },
+  { "empresa": 1, "totalVoosDomesticos": 1 }
+).limit(1);
