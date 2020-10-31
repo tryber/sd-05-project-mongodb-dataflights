dc03ea9c94a538c10613f2ddc6ed89fd9da47685
Marylange <marylangess@gmail.com>
2020-10-31 19:56:12 -0300

    CC2

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,6 +1,13 @@
 // Retorne o vooId do primeiro voo em 
 // que o campo rtk não exista.
 db.voos.find(
-  { "rtk": { $exists: false } },
-  { "_id": 0, "vooId": 1 }
+  {
+    "rtk": {
+      $exists: false
+    }
+  },
+  {
+    "_id": 0,
+    "vooId": 1
+  }
 ).limit(1);
