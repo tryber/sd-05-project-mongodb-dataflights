bf85983164096e77cac948c469d8853479425bbe
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-11-05 09:02:18 -0300

    desafio26 feito

---
+++ challenges/desafio26.js
@@ -0,0 +1,8 @@
+db.voos.deleteMany({
+  $and: [
+    { "empresa.nome": "AZUL" },
+    { "passageiros.pagos": { $gte: 5, $lte: 10 } },
+  ],
+});
+// transparencia: utilizado gabarito do dia de filter operators para entender
+// como pegar todos resultados entre 2 comparadores
