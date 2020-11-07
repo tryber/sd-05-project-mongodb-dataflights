02f852312d535fb6f07babcfbb9e568a9731cf6a
Eder Sena <eder.sena@live.com>
2020-11-07 18:44:58 -0300

    Desafio 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,1 +1,2 @@
-db.voos.find({"vooId": 756807}, {"empresa.sigla": 1, "empresa.name": 1, "passageiros": 1, "_id": 0});
+//Retorne o vooId, mes e ano do primeiro voo com mais de 7000 passageiros pagos.
+db.voos.find({"passageiros.pagos": {$gt: 7000}}, {"vooId": 1, "mes": 1, "ano": 1, "_id": 0}).limit(1);
