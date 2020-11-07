b66dac0834f87241d8c63a04cb951566b6c45de5
Eder Sena <eder.sena@live.com>
2020-11-07 19:42:15 -0300

    Desafio 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,17 @@
-db.voos.find({"vooId": 756807}, {"empresa.sigla": 1, "empresa.name": 1, "passageiros": 1, "_id": 0});
+// Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES,
+// a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK.
+db.voos.find({
+    $and: [{
+      $or:
+        [
+          { "empresa.nome": "DELTA AIRLINES" },
+          { "empresa.nome": "AMERICAN AIRLINES" },
+        ]
+    },
+    { "aeroportoOrigem.sigla": "SBGR" },
+    { "aeroportoDestino.sigla": "KJFK" },
+    ]
+  },
+    { "vooId": 1, "_id": 0 }
+  ).limit(1);
+  
