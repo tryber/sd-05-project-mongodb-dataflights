b972adcdb9dc7534d8aa0cb0bb32a5382d597f46
Eder Sena <eder.sena@live.com>
2020-11-07 19:54:37 -0300

    Desafio 23

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,2 +1,1 @@
-//Retorne o vooId do primeiro voo em que o campo litrosCombustivel exista
 db.voos.find({"litrosCombustivel":{$exists: true}},{"vooId": 1, "_id": 0 }).limit(1);
--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,2 +1,4 @@
-// Retorne o vooId do primeiro voo em que o campo rtk não exista.
-db.voos.find({"rtk":{$exists: false}},{"vooId": 1, "_id": 0 }).limit(1);
+db.voos.find({
+    "rtk":{$exists: false}},
+    {"vooId": 1, "_id": 0 }
+).limit(1);
