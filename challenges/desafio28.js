5f75002c2222b50575732f316c67a4882fda314c
Larissa Palombo <larissampalombo@gmail.com>
2020-11-09 20:06:43 -0300

    mais uma vez2

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,8 +1,8 @@
 db.resumoVoos.insertOne({
   empresa: "LATAM AIRLINES BRASIL",
   totalVoosDomesticos: db.voos.count({
-    "empresa.nome": "LATAM AIRLINES BRASIL",
     natureza: "Doméstica",
+    "empresa.nome": "LATAM AIRLINES BRASIL",
   }),
 });
 
