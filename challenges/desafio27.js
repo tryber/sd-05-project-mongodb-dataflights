33acd162d430a618f62fc586e6f08701c38730ea
Larissa Palombo <larissampalombo@gmail.com>
2020-11-09 17:38:58 -0300

    27

---
+++ challenges/desafio27.js
@@ -0,0 +1,11 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({
+    "empresa.nome": "PASSAREDO",
+    natureza: "Doméstica",
+  }),
+});
+db.resumoVoos.findOne(
+  { empresa: "PASSAREDO" },
+  { _id: 0, totalVoosDomesticos: 1, empresa: 1 }
+);
