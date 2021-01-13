72c0af2552ba5f031dcf2e37cbd2c6d17dfd2bf5
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-01-13 11:53:53 -0300

    something is not right

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,11 +1,7 @@
-db.voos.deleteMany(
-  {
-    $and: [
-      {
-        "empresa.nome": "GOL",
-        "passageiros.pagos": { $gte: 5 },
-        "passageiros.pagos": { $lte: 10 }
-      }
-    ]
-  }
-);
+db.voos.deleteMany({
+  $and: [
+    { "empresa.nome": "GOL" },
+    { "passageiros.pagos": { $gte: 5 } },
+    { "passageiros.pagos": { $lte: 10 } },
+  ],
+});
